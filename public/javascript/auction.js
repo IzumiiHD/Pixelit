ba93339ca5887da3c48f0c0b713fabf8034ad675
IzumiiHD <136212512+IzumiiHD@users.noreply.github.com>
2024-08-26 19:51:14 +0000

    re added the logout button

--- public/javascript/auction.js
+++ public/javascript/auction.js
@@ -1,25 +1,15 @@
-localStorage.setItem('tokens', users.tokens); // Replace 123 with actual token value
+const socket = io();
 
-document.addEventListener("DOMContentLoaded", () => {
-  const tokens = localStorage.getItem('tokens');
-  if (tokens !== null) {
-    document.getElementById('tokens').innerText = tokens;
-  } else {
-    console.error("No tokens found in localStorage");
-  }
-});
+if (localStorage.loggedin == "true") {
+  sessionStorage = localStorage;
+}
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetch('/user')  // Adjust this to your actual API endpoint
-    .then(response => response.json())
-    .then(data => {
-      const userRole = data.role;
-      const allowedRoles = ['Owner', 'Admin', 'Moderator', 'Helper'];
-      if (allowedRoles.includes(userRole)) {
-        document.getElementById('wrench-icon').style.display = 'inline';
-      }
-    })
-  .catch(error => {
-   console.error('Error fetching user role:', error);
-    });
-});
+function ge(id) {
+  return document.getElementById(id);
+}
+
+if (sessionStorage.loggedin == "true") {
+  username.innerHTML = " " + sessionStorage.username;
+  updateTokens();
+} else {
+}
