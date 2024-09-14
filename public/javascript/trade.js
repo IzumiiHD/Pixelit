7719d213e8dd95866e42410b8bb7e2e502acd8a4
aes256_encrypted <136212512+IzumiiHD@users.noreply.github.com>
2024-09-14 02:05:58 +0000

    wrench icon shows on the dashboard

--- public/javascript/trade.js
+++
@@ -1,8 +0,0 @@
-// Script to click on the element with id 'OG Pack' every 2 seconds
-setInterval(() => {
-    try {
-        document.getElementById('OG Pack').click();
-    } catch (error) {
-        console.log('Ignored an alert:', error.message);
-    }
-}, 2000);
