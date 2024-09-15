9fe5511dd0c1aa89f4560cb74f5b02a0b3132dd9
aes256_encrypted <136212512+IzumiiHD@users.noreply.github.com>
2024-09-15 20:33:55 +0000

    404 changes, owner panel edits and added a dashboard2.js file

---
+++ public/javascript/dashboard2.js
@@ -0,0 +1,23 @@
+document.getElementById('trade').addEventListener('click', function() {
+  alert('This feature will be added later upon release');
+});
+
+document.getElementById('viewStats').addEventListener('click', function() {
+  alert('This feature will be added later upon release');
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  fetch('/user') 
+    .then(response => response.json())
+    .then(data => {
+      const userRole = data.role;
+      const allowedRoles = ['Owner', 'Admin', 'Moderator', 'Helper'];
+      if (allowedRoles.includes(userRole)) {
+        document.getElementById('wrench-icon').style.display = 'inline';
+      }
+    })
+  .catch(error => {
+   console.error('Error fetching user role:', error);
+    });
+});
