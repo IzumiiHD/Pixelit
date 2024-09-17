d101b781e71c088c08a476fde7e9cdc8303b256b
aes256_encrypted <136212512+IzumiiHD@users.noreply.github.com>
2024-09-17 17:44:53 +0000

    making a hotter home page🔥

--- public/javascript/versionDisplay.js
+++ public/javascript/versionDisplay.js
@@ -13,5 +13,6 @@ export function displayVersion() {
   versionElement.style.color = '#f0f0f0';
   versionElement.style.fontFamily = 'Pixelify Sans, sans-serif';
   versionElement.style.fontSize = '0.75vw';
+  versionElement.style.textShadow = '2px 2px 4px rgba(0, 0, 0, 0.5)';
   document.body.appendChild(versionElement);
 }
